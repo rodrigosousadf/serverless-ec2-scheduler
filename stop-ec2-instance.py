7148a7e0a94826cabe0e502d174a4a63506cb701
Rodrigo Sousa <44593480+rodrigosousadf@users.noreply.github.com>
2024-02-26 20:21:54 -0300

    Create stop-ec2-instance.py

---
+++ stop-ec2-instance.py
@@ -0,0 +1,24 @@
+import boto3
+
+def lambda_handler(event, context):
+    # Inicializa o cliente EC2
+    ec2_client = boto3.client('ec2', region_name='ap-south-1') # Replace your Region name
+
+    # Especifica a instance Id da instancia EC2 que você quer pausar
+    instance_id = 'i-051262059d6f6350a' # Replace your Own Instance ID
+
+    # Pausa a instancia EC2
+    try:
+        response = ec2_client.stop_instances(InstanceIds=[instance_id], DryRun=False)
+        print(f"Stopping EC2 instance {instance_id}...")
+        print(f"Response: {response}")
+        return {
+            'statusCode': 200,
+            'body': f"EC2 instance {instance_id} is being stopped."
+        }
+    except Exception as e:
+        print(f"Error stopping EC2 instance: {e}")
+        return {
+            'statusCode': 500,
+            'body': f"Error stopping EC2 instance: {str(e)}"
+        }
