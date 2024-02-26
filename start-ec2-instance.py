c086afb771efa9e2555d92550449122185df2f61
Rodrigo Sousa <44593480+rodrigosousadf@users.noreply.github.com>
2024-02-26 20:19:56 -0300

    Create start-ec2-instance.py

---
+++ start-ec2-instance.py
@@ -0,0 +1,24 @@
+import boto3
+
+def lambda_handler(event, context):
+    # Inicializar o cliente EC2
+    ec2_client = boto3.client('ec2', region_name='ap-south-1') 
+
+    # Especifica a instance Id da EC2 que você quer iniciar
+    instance_id = 'i-051262059d6f6350a' # Replace your Own Instance ID
+
+    # Inicia a instância EC2
+    try:
+        response = ec2_client.start_instances(InstanceIds=[instance_id], DryRun=False)
+        print(f"Starting EC2 instance {instance_id}...")
+        print(f"Response: {response}")
+        return {
+            'statusCode': 200,
+            'body': f"EC2 instance {instance_id} is being started."
+        }
+    except Exception as e:
+        print(f"Error starting EC2 instance: {e}")
+        return {
+            'statusCode': 500,
+            'body': f"Error starting EC2 instance: {str(e)}"
+        }
